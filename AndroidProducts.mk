c56908d39b78e0c8381d4c355febb26729964703
Sundram <63789329+Yasundram@users.noreply.github.com>
2021-08-01 18:30:36 +0530

    RMX3171 : Initial CipherOS Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -16,3 +16,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_RMX3171.mk
+    
+COMMON_LUNCH_CHOICES := \
+        lineage_RMX3171-eng \
+        lineage_RMX3171-user \
+        lineage_RMX3171-userdebug
